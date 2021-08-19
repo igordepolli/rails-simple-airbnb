ee656e4623be764b88c5c6191ac4205ef5ff7289
Igor Depolli <igorcler@gmail.com>
2021-08-18 22:03:02 -0300

    Day 27 - Index flow implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+6.times do
+  Flat.create!(
+    name: Faker::Dessert.flavor,
+    address: Faker::Address.full_address,
+    description: Faker::Marketing.buzzwords,
+    price_per_night: rand(50..500),
+    number_of_guests: rand(1..8)
+  )
+end
